a77d3e5a198c363a47da299f8bda6480a4409dca
owleto <manchi.leung@gmail.com>
2015-01-22 15:14:28 +0800

    Initial commit

---
+++ src/main/java/owleto/HelloApp.java
@@ -0,0 +1,16 @@
+package owleto;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        HelloService helloService = context.getBean(HelloService.class);
+        System.out.println(helloService.sayHello());
+
+        System.out.println("end");
+        System.out.println("end2");
+        System.out.println("end3");
+    }
+}
